d48bd79fe5a894fef4c2eb8f6a37ee6c736f22a0
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2021-01-19 17:04:01 +0100

    Mark exercises

--- lib/Update.ml
+++ lib/Update.ml
@@ -12,12 +12,24 @@ module Make (P : Monoid) (S : MonoidAction with type m = P.t) = struct
   module Reader = Reader.Make(struct type t = S.t end)
   module Writer = Writer.Make(P)
 
+  (* sujet
+  let lift ((l1, k): 'a Reader.t Writer.t): 'a Writer.t Reader.t = failwith "NYI"
+     /sujet *)
+  (* corrige *)
   let lift ((l1, k): 'a Reader.t Writer.t): 'a Writer.t Reader.t =
     fun s -> (l1, k (S.act s l1))
+  (* /corrige *)
 
   module Base = struct
     type 'a t = 'a Writer.t Reader.t
 
+    (* sujet
+    let return a = failwith "NYI"
+
+    let bind m f = failwith "NYI"
+       /sujet *)
+
+    (* corrige *)
     let return a = Reader.return (Writer.return a)
 
     let bind (rwm: 'a t)(f: 'a -> 'b t) =
@@ -25,19 +37,27 @@ module Make (P : Monoid) (S : MonoidAction with type m = P.t) = struct
           Reader.bind (lift (Writer.bind wm (fun v ->
                     Writer.return (f v)))) (fun rm ->
               Reader.return (Writer.join rm)))
-
+    (* /corrige *)
   end
 
   module M = Monad.Expand (Base)
   include M
   open Base
 
+  (* sujet
+  let get () = failwith "NYI"
+  let set s = failwith "NYI"
+  let run m s0 = failwith "NYI"
+     /sujet *)
+
+  (* corrige *)
   let get () = Reader.bind (Reader.get ()) return
 
-  let set a = lift (Writer.bind (Writer.set a) (fun _ ->
+  let set s = lift (Writer.bind (Writer.set s) (fun _ ->
                         Writer.return (Reader.return ())))
 
   let run m s0 = Writer.run (Reader.run m s0)
+  (* /corrige *)
 
 
 end
